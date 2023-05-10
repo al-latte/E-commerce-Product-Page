1cfeabefce0ef66ea329e51b224c65f8eebee532
al-latte <robertskarlisha@gmail.com>
2023-05-10 18:35:50 -0400

    import images in images.tsx file to fix image not displaying issue.

--- src/images.tsx
+++ src/images.tsx
@@ -1,19 +1,29 @@
+// product display product images
+import productOne from "./src/assets/images/image-product-1.jpg";
+import productTwo from "./src/assets/images/image-product-2.jpg";
+import productThree from "./src/assets/images/image-product-3.jpg";
+import productFour from "./src/assets/images/image-product-4.jpg";
+// product display product thumbnail images
+import productThumbOne from "./src/assets/images/image-product-1-thumbnail.jpg";
+import productThumbTwo from "./src/assets/images/image-product-2-thumbnail.jpg";
+import productThumbThree from "./src/assets/images/image-product-3-thumbnail.jpg";
+import productThumbFour from "./src/assets/images/image-product-4-thumbnail.jpg";
+// Icons
+import close from "./src/assets/images/icon-close.svg";
+import previous from "./src/assets/images/icon-previous.svg";
+import next from "./src/assets/images/icon-next.svg";
+
 const productDisplayImages = {
-	displayImages: [
-		"src/assets/images/image-product-1.jpg",
-		"src/assets/images/image-product-2.jpg",
-		"src/assets/images/image-product-3.jpg",
-		"src/assets/images/image-product-4.jpg",
-	],
+	displayImages: [productOne, productTwo, productThree, productFour],
 	thumbnailImages: [
-		"src/assets/images/image-product-1-thumbnail.jpg",
-		"src/assets/images/image-product-2-thumbnail.jpg",
-		"src/assets/images/image-product-3-thumbnail.jpg",
-		"src/assets/images/image-product-4-thumbnail.jpg",
+		productThumbOne,
+		productThumbTwo,
+		productThumbThree,
+		productThumbFour,
 	],
-	close: "src/assets/images/icon-close.svg",
-	previous: "src/assets/images/icon-previous.svg",
-	next: "src/assets/images/icon-next.svg",
+	close: close,
+	previous: previous,
+	next: next,
 };
 
 export { productDisplayImages };
