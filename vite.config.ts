419f92800376e6bdf4a8599ee61d4c997be1d96c
al-latte <robertskarlisha@gmail.com>
2023-05-09 21:14:18 -0400

    base changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "https://github.com/al-latte/E-commerce-Product-Page",
+	base: "/E-commerce-Product-Page/",
 	plugins: [react()],
 });
