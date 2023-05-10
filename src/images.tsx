652bdc53e2b3278e8241f98d627637c1bc3df32b
al-latte <robertskarlisha@gmail.com>
2023-05-10 18:55:47 -0400

    images issue fix?

--- src/images.tsx
+++ src/images.tsx
@@ -1,17 +1,17 @@
 // product display product images
-import productOne from "./src/assets/images/image-product-1.jpg";
-import productTwo from "./src/assets/images/image-product-2.jpg";
-import productThree from "./src/assets/images/image-product-3.jpg";
-import productFour from "./src/assets/images/image-product-4.jpg";
+import productOne from "/src/assets/images/image-product-1.jpg";
+import productTwo from "/src/assets/images/image-product-2.jpg";
+import productThree from "/src/assets/images/image-product-3.jpg";
+import productFour from "/src/assets/images/image-product-4.jpg";
 // product display product thumbnail images
-import productThumbOne from "./src/assets/images/image-product-1-thumbnail.jpg";
-import productThumbTwo from "./src/assets/images/image-product-2-thumbnail.jpg";
-import productThumbThree from "./src/assets/images/image-product-3-thumbnail.jpg";
-import productThumbFour from "./src/assets/images/image-product-4-thumbnail.jpg";
+import productThumbOne from "/src/assets/images/image-product-1-thumbnail.jpg";
+import productThumbTwo from "/src/assets/images/image-product-2-thumbnail.jpg";
+import productThumbThree from "/src/assets/images/image-product-3-thumbnail.jpg";
+import productThumbFour from "/src/assets/images/image-product-4-thumbnail.jpg";
 // Icons
-import close from "./src/assets/images/icon-close.svg";
-import previous from "./src/assets/images/icon-previous.svg";
-import next from "./src/assets/images/icon-next.svg";
+import close from "/src/assets/images/icon-close.svg";
+import previous from "/src/assets/images/icon-previous.svg";
+import next from "/src/assets/images/icon-next.svg";
 
 const productDisplayImages = {
 	displayImages: [productOne, productTwo, productThree, productFour],
